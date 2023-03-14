51ccac5ca30f8f21926f1d659f1825f742344584
Mihkel Väli <mihkel@bobw.co>
2023-03-14 19:42:39 +0200

    change trek details in admin

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,6 @@ const port = process.env.PORT || 3333;
 app.use(cors());
 app.use(express.json());
 
-app.get("/treks/:trekId", async (req, res) => {
-  const sql = `SELECT * FROM treks WHERE id=${req.params.trekId};`
-  console.log(sql);
-  const { rows } = await pool.query(sql);
-  res.send(JSON.stringify(rows[0]));
-});
-
 app.get("/treks", async (req, res) => {
   const columns = req.query.columns;
   const status = req.query.status;
@@ -31,7 +24,31 @@ app.get("/treks", async (req, res) => {
     sqlAdditionalFilters += `WHERE status='${status}'`
   }
 
-  sql = `SELECT ${sqlColumns} FROM treks ${sqlAdditionalFilters};`
+  sql = `SELECT ${sqlColumns} FROM treks ${sqlAdditionalFilters} ORDER BY id;`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
+  res.send(JSON.stringify(rows));
+});
+
+app.get("/treks/:trekId", async (req, res) => {
+  const sql = `SELECT * FROM treks WHERE id=${req.params.trekId} ORDER BY id;`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
+  res.send(JSON.stringify(rows[0]));
+});
+
+app.put("/treks/:trekId", async (req, res) => {
+  console.log(req.body);
+  const sql = `
+    UPDATE treks
+    SET
+      title='${req.body.title}',
+      description='${req.body.description}',
+      image_url='${req.body.image_url}',
+      duration='${req.body.duration}',
+      status='${req.body.status}
+    WHERE id=${req.params.trekId};
+  `
   console.log(sql);
   const { rows } = await pool.query(sql);
   res.send(JSON.stringify(rows));
