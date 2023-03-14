50237669e9af349997b2337c94b965b5d2bb83ba
Mihkel Väli <mihkel@bobw.co>
2023-03-14 18:13:35 +0200

    matkaklubi algus

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 import pg from "pg";
 
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
 const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(cors());
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.get("/treks", async (req, res) => {
+  const status = req.query.status;
+  let sqlAdditionalFilters = '';
+  if (status !== undefined) {
+    sqlAdditionalFilters += `WHERE status = ${status}`
+  }
+
+  const { rows } = await pool.query(`SELECT * FROM treks ${sqlAdditionalFilters};`);
+  res.send(JSON.stringify(rows));
 });
 
 app.listen(port, () => {
