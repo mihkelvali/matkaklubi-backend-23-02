cee0f1874a73d78c9bec1e5312c3ccd59f43c099
Mihkel Väli <mihkel@bobw.co>
2023-03-14 18:27:45 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/treks", async (req, res) => {
+  const columns = req.query.columns;
   const status = req.query.status;
+  let sql = '';
+  let sqlColumns = '*';
   let sqlAdditionalFilters = '';
+
+  if (columns !== undefined) {
+    sqlColumns = columns.toString();
+  }
   if (status !== undefined) {
-    sqlAdditionalFilters += `WHERE status = ${status}`
+    sqlAdditionalFilters += `WHERE status='${status}'`
   }
 
-  const { rows } = await pool.query(`SELECT * FROM treks ${sqlAdditionalFilters};`);
+  sql = `SELECT ${sqlColumns} FROM treks ${sqlAdditionalFilters};`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
   res.send(JSON.stringify(rows));
 });
 
